7109ed05d6710f0178f4e6ab2b655d0901f0d38a
bubusy <bubusy@163.com>
2022-07-25 01:08:12 +0800

    loading

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
 import Footer from './footer';
 import Header from './header';
 
 export default function Layout({ children }) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => setTimeout(() => setLoading(false), 1600), []);
+  if (loading) {
+    return (
+      <div className='flex h-[100vh] items-center justify-center'>
+        <img src='/litepaper/images/tars-protocol-gif.gif' alt='' />
+      </div>
+    );
+  }
   return (
     <>
       <Header />
