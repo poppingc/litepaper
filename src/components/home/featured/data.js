2185a161f4ac4caecff243bcb1d7f56d5376e473
bubusy <bubusy@163.com>
2022-08-07 23:26:47 +0800

    path

--- src/components/home/featured/data.js
+++ src/components/home/featured/data.js
@@ -1,6 +1,6 @@
 export const data = [
   {
-    logo: '/images/featured-icons/ap.jpg',
+    logo: '/litepaper/images/featured-icons/ap.jpg',
     link: 'https://apnews.com/press-release/marketersmedia/space-launches-2e98c1db6470cb80692b102e0da654e2',
     twitter: 'https://twitter.com/AP',
     twitter_followers: '1',
@@ -9,7 +9,7 @@ export const data = [
       'TARS Protocol launches Space to promote more trustable brands on Web3',
   },
   {
-    logo: '/images/featured-icons/yahoo-news.png',
+    logo: '/litepaper/images/featured-icons/yahoo-news.png',
     link: 'https://news.yahoo.com/tars-protocol-completed-security-audit-171300372.html',
     twitter: 'https://twitter.com/YahooNews',
     twitter_followers: '1',
@@ -17,7 +17,7 @@ export const data = [
     title: 'TARS Protocol Has Completed the Security Audit Report by DeHacker',
   },
   {
-    logo: '/images/featured-icons/yahoo-finance.png',
+    logo: '/litepaper/images/featured-icons/yahoo-finance.png',
     link: 'https://finance.yahoo.com/news/tars-protocol-completed-security-audit-171300372.html',
     twitter: 'https://twitter.com/YahooFinance',
     twitter_followers: '1',
@@ -25,7 +25,7 @@ export const data = [
     title: 'TARS Protocol Has Completed the Security Audit Report by DeHacker',
   },
   {
-    logo: '/images/featured-icons/newsmax.png',
+    logo: '/litepaper/images/featured-icons/newsmax.png',
     link: 'https://pr.newsmax.com/article/TARS-Protocol-launches-Space-to-promote-more-trustable-brands-on-Web3?storyId=62bae0d0b8c15cb11c2c04b7',
     twitter: 'https://twitter.com/Newsmax',
     twitter_followers: '1',
@@ -34,7 +34,7 @@ export const data = [
       'TARS Protocol launches Space to promote more trustable brands on Web3',
   },
   {
-    logo: '/images/featured-icons/marketwatch.svg',
+    logo: '/litepaper/images/featured-icons/marketwatch.svg',
     link: 'https://www.marketwatch.com/press-release/tars-protocol-has-completed-the-security-audit-report-by-dehacker-2022-07-05?mod=search_headline',
     twitter: 'https://twitter.com/MarketWatch',
     twitter_followers: '1',
@@ -42,7 +42,7 @@ export const data = [
     title: 'TARS Protocol Has Completed the Security Audit Report by DeHacker',
   },
   {
-    logo: '/images/featured-icons/azcentral.svg',
+    logo: '/litepaper/images/featured-icons/azcentral.svg',
     link: 'https://www.benzinga.com/pressreleases/22/07/n20350681/tars-protocol-has-completed-the-security-audit-report-by-dehacker',
     twitter: 'https://twitter.com/Benzinga',
     twitter_followers: '1',
@@ -50,7 +50,7 @@ export const data = [
     title: 'TARS Protocol Has Completed the Security Audit Report by DeHacker',
   },
   {
-    logo: '/images/featured-icons/benzinga.svg',
+    logo: '/litepaper/images/featured-icons/benzinga.svg',
     link: 'http://finance.azcentral.com/azcentral/article/getnews-2022-7-14-paul-xu-ceo-of-klein-labs-introducing-how-tars-protocol-brings-the-paradigm-shift-to-the-field-of-crypto-investment/',
     twitter: 'https://twitter.com/azcentral',
     twitter_followers: '1',
@@ -59,7 +59,7 @@ export const data = [
       'Paul Xu, CEO of Klein Labs, introducing how TARS protocol brings the paradigm shift to the field of crypto investment.',
   },
   {
-    logo: '/images/featured-icons/buffalo_news_logo.svg',
+    logo: '/litepaper/images/featured-icons/buffalo_news_logo.svg',
     link: 'http://markets.buffalonews.com/buffnews/news/article/marketersmedia-2022-6-28-tars-protocol-launches-space-to-promote-more-trustable-brands-on-web3',
     twitter: 'https://twitter.com/TheBuffaloNews',
     twitter_followers: '1',
@@ -68,7 +68,7 @@ export const data = [
       'TARS Protocol launches Space to promote more trustable brands on Web3',
   },
   {
-    logo: '/images/featured-icons/sportify.png',
+    logo: '/litepaper/images/featured-icons/sportify.png',
     link: 'http://sportifynews.com/news/tars-protocol-has-completed-the-security-audit-report-by-dehacker/0390976/',
     twitter: 'https://twitter.com/SpotifyNews',
     twitter_followers: '1',
@@ -76,7 +76,7 @@ export const data = [
     title: 'TARS Protocol Has Completed the Security Audit Report by DeHacker',
   },
   {
-    logo: '/images/featured-icons/pittsburgh-post-gazette.png',
+    logo: '/litepaper/images/featured-icons/pittsburgh-post-gazette.png',
     link: 'http://markets.post-gazette.com/postgazette/news/article/marketersmedia-2022-6-28-tars-protocol-launches-space-to-promote-more-trustable-brands-on-web3',
     twitter: 'https://twitter.com/PittsburghPG',
     twitter_followers: '1',
@@ -85,7 +85,7 @@ export const data = [
       'TARS Protocol launches Space to promote more trustable brands on Web3',
   },
   {
-    logo: '/images/featured-icons/wedbush.svg',
+    logo: '/litepaper/images/featured-icons/wedbush.svg',
     link: 'http://investor.wedbush.com/wedbush/article/getnews-2022-7-14-paul-xu-ceo-of-klein-labs-introducing-how-tars-protocol-brings-the-paradigm-shift-to-the-field-of-crypto-investment/',
     twitter: 'https://twitter.com/wedbush',
     twitter_followers: '1',
@@ -94,7 +94,7 @@ export const data = [
       'Paul Xu, CEO of Klein Labs, introducing how TARS protocol brings the paradigm shift to the field of crypto investment.',
   },
   {
-    logo: '/images/featured-icons/herald.svg',
+    logo: '/litepaper/images/featured-icons/herald.svg',
     link: 'http://finance.dailyherald.com/dailyherald/article/getnews-2022-6-23-tars-an-automated-web3-protocol-has-launched-compound-decoupling-technology-of-web3/',
     twitter: 'https://twitter.com/dailyherald',
     twitter_followers: '1',
@@ -103,7 +103,7 @@ export const data = [
       'TARS, an automated Web3 protocol, has launched compound decoupling technology of web3',
   },
   {
-    logo: '/images/featured-icons/sherbrooke.png',
+    logo: '/litepaper/images/featured-icons/sherbrooke.png',
     link: 'http://business.sherbrookerecord.com/sherbrookerecord/article/getnews-2022-7-14-paul-xu-ceo-of-klein-labs-introducing-how-tars-protocol-brings-the-paradigm-shift-to-the-field-of-crypto-investment/',
     twitter: 'https://twitter.com/recordnewspaper',
     twitter_followers: '1',
@@ -112,7 +112,7 @@ export const data = [
       'Paul Xu, CEO of Klein Labs, introducing how TARS protocol brings the paradigm shift to the field of crypto investment.',
   },
   {
-    logo: '/images/featured-icons/Digital-Journal.png',
+    logo: '/litepaper/images/featured-icons/Digital-Journal.png',
     link: 'http://digitaljournal.com/pr/tars-an-automated-web3-protocol-has-launched-compound-decoupling-technology-of-w',
     twitter: 'https://twitter.com/digitaljournal',
     twitter_followers: '1',
@@ -121,7 +121,7 @@ export const data = [
       'TARS, an automated Web3 protocol, has launched compound decoupling technology of web3',
   },
   {
-    logo: '/images/featured-icons/britishfuture.png',
+    logo: '/litepaper/images/featured-icons/britishfuture.png',
     link: 'http://www.britishft.com/press/tars-protocol-has-completed-the-security-audit-report-by-dehacker-9069/',
     twitter: 'https://twitter.com/britishfuture',
     twitter_followers: '1',
