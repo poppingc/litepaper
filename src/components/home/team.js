cdca3ebea885d7052640461a2d7fab5fd67121f5
poppingc <59042360+poppingc@users.noreply.github.com>
2022-08-07 21:59:18 +0800

    Update team.js

--- src/components/home/team.js
+++ src/components/home/team.js
@@ -2,7 +2,7 @@ const teamMembers = [
   {
     name: 'Paul Xu',
     title: 'Co-Founder & CEO',
-    linkedin: 'https://www.linkedin.com/in/paul-xu-696b55215/)',
+    linkedin: 'https://www.linkedin.com/in/paul-xu-696b55215',
     twitter: 'https://twitter.com/Paulxuxu',
     telegram: 'https://t.me/Paul_Xuu',
     description:
@@ -12,7 +12,7 @@ const teamMembers = [
   {
     name: 'Joesph Chueng',
     title: 'Co-Founder & CSO',
-    linkedin: 'https://www.linkedin.com/in/m-joesph-cheung-223691103/',
+    linkedin: 'https://www.linkedin.com/in/m-joesph-cheung-223691103',
     telegram: 'https://t.me/bigcatfi',
     description:
       'Joesph is an entrepreneur, crypto investor, and start-up incubator. He has been engaging in research about blockchain protocols and DeFi since 2017 and has seasoned experience in scaling startups from 0 to 1. Joesph is responsible for business strategy and he leads the business team of TARS.',
@@ -22,7 +22,7 @@ const teamMembers = [
   {
     name: 'William Sarbinowski',
     title: 'Head of Business Development',
-    linkedin: 'https://www.linkedin.com/in/william-sarbinowski-2447351ab/',
+    linkedin: 'https://www.linkedin.com/in/william-sarbinowski-2447351ab',
     telegram: '',
     description:
       'William is from Cornell University and Cornell Blockchain Club. His field of study is mathematics and computer science. Prior to joining TARS, he gained experience by working on business development at both biotech and financial advising companies. He is leading the business development at TARS.',
@@ -42,7 +42,7 @@ const teamMembers = [
   {
     name: 'Chiev Wan',
     title: 'Product Manager',
-    linkedin: '',
+    linkedin: 'https://www.linkedin.com/in/chiver-wan',
     telegram: '',
     description:
       'Chiev is from Carnegie Mellon University, and his field of study is Electrical and Computer Engineering. Before he got interested in blockchain, Chiev used to work as the software engineer for tech firms, such as Bytedance. He has abundant experience developing smart contracts.',
@@ -71,15 +71,18 @@ const teamMembers = [
 export default function HomeTeam() {
   return (
     <div className='site-container'>
-      <div id='team' className='mx-auto md:max-w-4xl  '>
+      <div id='team' className='mx-auto md:max-w-4xl'>
         <h4 className='text-center text-3xl font-extrabold text-white lg:text-4xl xl:text-5xl '>
           Team <span className='text-green-primary'>Members</span>
         </h4>
       </div>
 
-      <div className='mt-16 grid justify-center gap-12 sm:grid-cols-4 md:mt-20 '>
+      <div className='mt-16 grid justify-center gap-12 text-center sm:grid-cols-3 sm:text-left md:mt-20 md:grid-cols-4 '>
         {teamMembers.map((item, index) => (
-          <div className='w-full max-w-[280px] ' key={index}>
+          <div
+            className='flex w-full max-w-[280px] flex-col items-center sm:block'
+            key={index}
+          >
             {item.photo && (
               <img
                 src={item.photo}
@@ -87,7 +90,7 @@ export default function HomeTeam() {
                 className='mb-5 h-[150px] w-[150px] shrink-0 rounded-full object-contain transition ease-in hover:-translate-y-2'
               />
             )}
-            <h4 className='text-lg font-bold text-white md:text-xl lg:text-2xl '>
+            <h4 className='whitespace-nowrap text-lg font-bold text-white md:text-xl lg:text-2xl '>
               {item.name}
             </h4>
             <div className='mt-4 flex items-center space-x-2 '>
